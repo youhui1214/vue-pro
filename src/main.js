9e12204f5abde34e496f75f7351c75cc7f86ea9a
尤辉 <405852890@qq.com>
2021-03-29 13:29:26 +0800

    feat: test

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css/normalize.css'
-import Api from './api/index.js';
-Vue.prototype.$api = Api;
+// Vue.prototype.$api = Api;
 
 
 Vue.config.productionTip = false
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <div class="header">
+            <div class="left"></div>
+            <div class="right"></div>
+
+        </div>
+
     </div>
 </template>
 
 <script>
 
 import HelloWorld from '@/components/HelloWorld.vue'
+import MainItem from '@/components/MainItem.vue'
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld
+        HelloWorld,
+        MainItem
     }
 }
 </script>
+
+<style scoped>
+
+.home {
+    width: 100%;
+    height: 100%;
+    background-color: antiquewhite;
+}
+.header {
+    width: 100%;
+    height: 100px;
+    background-color: #42b983;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.left {
+    margin-left: 20px;
+    width: 100px;
+    height: 60px;
+    background-color: aqua;
+}
+
+.right {
+    margin-right: 20px;
+    width: 100px;
+    height: 80px;
+    background-color: aquamarine;
+}
+
+
+
+</style>
